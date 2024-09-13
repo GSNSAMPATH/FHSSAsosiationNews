c5404fff99da7b89ec94ad51ee88be5dcf6c49bf
Sampath G.S.N <90971404+GSNSAMPATH@users.noreply.github.com>
2024-09-13 15:35:10 +0530

    update index.ts6

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,42 +1,44 @@
 import { Request, Response } from 'express';
-import User from '../models/userModel';
-import bcrypt from 'bcrypt';
+// import User from '../models/userModel';
+// import bcrypt from 'bcrypt';
 
 
 
 // Register a new user
 export const registerUser = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { name, registationNumber, email, password,image } = req.body;
 
-        // Check if the user already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            res.status(400).json({ message: 'User already exists' });
-            return;
-        }
+    res.send('registerUser');
+//     try {
+//         const { name, registationNumber, email, password,image } = req.body;
 
-        // Hash the password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+//         // Check if the user already exists
+//         const existingUser = await User.findOne({ email });
+//         if (existingUser) {
+//             res.status(400).json({ message: 'User already exists' });
+//             return;
+//         }
 
-        // Create a new user
-        const user = new User({
-            name,
-            registationNumber,
-            email,
-            password: hashedPassword,
-            image,
-        });
+//         // Hash the password
+//         const salt = await bcrypt.genSalt(10);
+//         const hashedPassword = await bcrypt.hash(password, salt);
 
-        // Save the user to the database
-        await user.save();
+//         // Create a new user
+//         const user = new User({
+//             name,
+//             registationNumber,
+//             email,
+//             password: hashedPassword,
+//             image,
+//         });
 
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'Server error', error });
-    }
-};
+//         // Save the user to the database
+//         await user.save();
+
+//         res.status(201).json({ message: 'User registered successfully' });
+//     } catch (error) {
+//         res.status(500).json({ message: 'Server error', error });
+//     }
+ };
 
 // Login a user
 // export const loginUser = async (req: Request, res: Response): Promise<void> => {
