228b5f3657c83e1cf680749fbaeeca00407d006b
KaweeshaHerath <ar102224@fhss.sjp.ac.lk>
2024-09-17 14:40:25 +0530

    Added CSS import, renamed comment, and refactored div styling to use CSS class

--- web/pitaNews/src/page/Home.tsx
+++ web/pitaNews/src/page/Home.tsx
@@ -2,22 +2,26 @@ import { auth } from '../Authotication/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 import Flower from '../components/background'
 import Navbar from '../routs/Navebar'
+import './Home.css'; //Importing Css File here
 
 export default function Home() {
 
   onAuthStateChanged(auth, (user) => {
     if (!user) {
-      // Using to un athentcating users
+      // Using for an un-athentcating users
     }
   })
 
   return (
     <>
     <Navbar />
+    <div className ="home-container">
+
+    <Flower />
     <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: '20px', padding: '20px' }}>
     
-  
-    <Flower />
+    
+    </div>
     </div>
     </>
   )
