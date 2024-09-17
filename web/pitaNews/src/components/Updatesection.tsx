1876f32a2ece859983da77e7723298f5e24cbea9
KaweeshaHerath <ar102224@fhss.sjp.ac.lk>
2024-09-17 14:46:15 +0530

    Added UpdatesSection component with testing image elements

---
+++ web/pitaNews/src/components/Updatesection.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const UpdatesSection: React.FC = () => {
+  return (
+    <div className="section">
+      <h2>Lateset Updates</h2>
+      <div className="content">
+        <div className="large">
+          <img
+            alt="Large sports image"
+            height="400"
+            src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-LmQ09WWGIGwOeeA4ArnRw0x5/user-uJPET5fjNenSso8wCETWVNOp/img-Oj0A6oAtuJvLAhY5KeesqOCE.png"
+            width="600"
+          />
+        </div>
+        <div className="small">
+          <img
+            alt="Small sports image 1"
+            height="200"
+            src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-LmQ09WWGIGwOeeA4ArnRw0x5/user-uJPET5fjNenSso8wCETWVNOp/img-W5HzB7avpOYgTmHww9BlIx0g.png"
+            width="200"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UpdatesSection;
