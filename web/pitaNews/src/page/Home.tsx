19b0028bad5f600473410ec10388c703fc112871
KaweeshaHerath <ar102224@fhss.sjp.ac.lk>
2024-09-17 14:07:30 +0530

    Removed React and useNavigate imports, and a navigate function call in Home component

--- web/pitaNews/src/page/Home.tsx
+++ web/pitaNews/src/page/Home.tsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
 import { auth } from '../Authotication/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 import Flower from '../components/background'
 import Navbar from '../routs/Navebar'
 
 export default function Home() {
-  const navigate = useNavigate()
 
   onAuthStateChanged(auth, (user) => {
     if (!user) {
-      
+      // Using to un athentcating users
     }
   })
 
