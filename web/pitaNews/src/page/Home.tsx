630af92c93fbf05a27c5f13ab38409dc68e6bbb1
KaweeshaHerath <ar102224@fhss.sjp.ac.lk>
2024-09-18 09:26:24 +0530

    Removed PostsSection and UpdatesSection components, and re coded Home.css and Home.tsx files.

--- web/pitaNews/src/page/Home.tsx
+++ web/pitaNews/src/page/Home.tsx
@@ -1,8 +1,12 @@
+import React from 'react';
 import { auth } from '../Authotication/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 import Flower from '../components/background'
 import Navbar from '../routs/Navebar'
 import './Home.css'; //Importing Css File here
+import UpdatesSection from '../components/UpdatesSection'
+import NewsSection from '../components/Newssection'
+import PostsSection from '../components/PostsSection'
 
 export default function Home() {
 
@@ -19,7 +23,9 @@ export default function Home() {
 
     <Flower />
     <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', gap: '20px', padding: '20px' }}>
-    
+      <UpdatesSection/>
+      <NewsSection/>
+      <PostsSection/>
     
     </div>
     </div>
